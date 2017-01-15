3b4cdbc17f2f4dad4ff40af69b0b6ee9e8d81e18
mlschan <michellelschan@gmail.com>
2017-01-14 23:42:42 -0800

    Move Playlist out of index.js

--- src/index.js
+++ src/index.js
@@ -2,101 +2,12 @@
 var firebase = require('firebase');
 var fs = require('fs');
 
-//var Playlist = require('./playlist');
-class Playlist {
-
-    constructor(playlistId, currentIndex) {
-        this.playlistId = playlistId;
-        this.currentIndex = currentIndex;
-    }
-
-    playFirst(event, context) {
-        this.currentIndex = 0;
-        this.buildAudioPlayerResponse(event, context, 0, true);
-    }
-
-    playPrevious(event, context) {
-        this.buildAudioPlayerResponse(event, context, -1, true);
-    }
-
-    playNext(event, context) {
-        this.buildAudioPlayerResponse(event, context, 1, true);
-    }
-
-    queuePrevious(event, context) {
-        this.buildAudioPlayerResponse(event, context, -1, false);
-    }
-
-    queueNext(event, context) {
-        this.buildAudioPlayerResponse(event, context, 1, false);
-    }
-
-    buildAudioPlayerResponse(event, context, indexOffset, isPlayImmediately) {
-        console.log("queue");
-        this.getSongDownloadUrls()
-            .then((urls) => {
-                if (urls[this.currentIndex + indexOffset]) {
-                    this.currentIndex += indexOffset;
-                    respond(
-                        /*context:*/ context,
-                        /*spokenMessage:*/ null,
-                        /*cardMessage:*/ null,
-                        /*audioUrl:*/ urls[this.currentIndex],
-                        /*playBehavior:*/ isPlayImmediately ? "REPLACE_ALL"  : "ENQUEUE",
-                        /*token:*/ this.getTokenJson(),
-                        /*expectedPreviousToken:*/ event.context.AudioPlayer.token,
-                        /*shouldEndSession:*/ true,
-                        /*isStop:*/ false,
-                        /*isClearQueue:*/ false);
-                } else {
-                    respond(
-                        /*context:*/ context,
-                        /*spokenMessage:*/ null,
-                        /*cardMessage:*/ null,
-                        /*audioUrl:*/ null,
-                        /*playBehavior:*/ null,
-                        /*token:*/ this.getTokenJson(),
-                        /*expectedPreviousToken:*/ null,
-                        /*shouldEndSession:*/ true,
-                        /*isStop:*/ false,
-                        /*isClearQueue:*/ false);
-                }
-            });
-    }
-
-    getSongDownloadUrls() {
-        let songIds;
-        let downloadUrls;
-        // log-in to firebase
-        return firebase.auth().signInWithEmailAndPassword(FIREBASE_USERNAME, FIREBASE_PASSWORD)
-            // fetch songs in playlist
-            .then(() => firebase.database().ref(`playlists/${this.playlistId}/songs`).once('value'))
-            // extract songIds from songs in playlist
-            // fetch all songs
-            .then(snapshot => {
-                var playlistSongs = snapshot.val();
-                songIds = Object.keys(playlistSongs).map(playlistSongKey => playlistSongs[playlistSongKey].songId);
-                return firebase.database().ref('songs').once('value');
-            // use songIds to extract downloadUrls from songs
-            }).then(snapshot => {
-                var songs = snapshot.val();
-                downloadUrls = songIds.map(songId => songs[songId].downloadUrl);
-                return firebase.auth().signOut();
-            }).then(() => downloadUrls);
-    }
-
-    getTokenJson() {
-        return JSON.stringify({
-            playlistId: this.playlistId,
-            currentIndex: this.currentIndex
-        });
-    }
-}
+var Playlist = require('./playlist');
 
 let ALEXA_CONFIG = require('./_no_commit/alexa-config.json');
-let APP_ID = ALEXA_CONFIG.ID;
-let APP_TEST_ID = ALEXA_CONFIG.TEST_ID;
-let APP_NAME = ALEXA_CONFIG.NAME;
+let ALEXA_SKILL_ID = ALEXA_CONFIG.ID;
+let ALEXA_SKILL_TEST_ID = ALEXA_CONFIG.TEST_ID;
+let ALEXA_SKILL_NAME = ALEXA_CONFIG.NAME;
 
 let FIREBASE_CONFIG = require('./_no_commit/firebase-config.json');
 let FIREBASE_USERNAME = FIREBASE_CONFIG.USER.USERNAME;
@@ -115,11 +26,8 @@ exports.handler = function (event, context, callback) {
 
     if (event.session
         && event.session.application
-        && event.session.application.applicationId !== APP_ID
-        && event.session.application.applicationId !== APP_TEST_ID) {
-            console.log(event.session);
-            console.log(APP_ID);
-            console.log(APP_TEST_ID);
+        && event.session.application.applicationId !== ALEXA_SKILL_ID
+        && event.session.application.applicationId !== ALEXA_SKILL_TEST_ID) {
         context.fail("ERROR: Invalid Application ID");
     }
     else {
@@ -266,7 +174,7 @@ function onIntentRequest(event, context, callback) {
         case "AMAZON.YesIntent":
             respond(
                 /*context:*/ context,
-                /*spokenMessage:*/ `${APP_NAME} didn't expect to get an IntentRequest with name ${event.request.intent.name}.`,
+                /*spokenMessage:*/ `${ALEXA_SKILL_NAME} didn't expect to get an IntentRequest with name ${event.request.intent.name}.`,
                 /*cardMessage:*/ null,
                 /*audioUrl:*/ null,
                 /*playBehavior:*/ null,
@@ -290,7 +198,7 @@ function onIntentRequest(event, context, callback) {
         default:
             respond(
                 /*context:*/ context,
-                /*spokenMessage:*/ `${APP_NAME} didn't expect to get an IntentRequest with name ${event.request.intent.name}.`,
+                /*spokenMessage:*/ `${ALEXA_SKILL_NAME} didn't expect to get an IntentRequest with name ${event.request.intent.name}.`,
                 /*cardMessage:*/ null,
                 /*audioUrl:*/ null,
                 /*playBehavior:*/ null,
@@ -383,7 +291,7 @@ function buildResponseJSON(
         responseBody.response.card =
             {
                 type: "Simple",
-                title: APP_NAME,
+                title: ALEXA_SKILL_NAME,
                 content: cardMessage,
             };
     }
