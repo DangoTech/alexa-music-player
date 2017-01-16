0b4e1298352a2cf9f6b5e7042314ca9c9b6ea3d8
mlschan <michellelschan@gmail.com>
2017-01-15 18:49:21 -0800

    Convert from 4 space indents to 2 space indents.  Also change all double-quotes to single-quotes in javascript files.

--- src/alexa-service.js
+++ src/alexa-service.js
@@ -1,119 +1,122 @@
-"use strict";
+'use strict';
+let ALEXA_CONFIG = require('./_no_commit/alexa-config.json');
+let ALEXA_SKILL_NAME = ALEXA_CONFIG.NAME;
+
 module.exports = class AlexaService {
-	static respondWithEndSession(context) {
-	    this.respond(
-	        /*context:*/ context,
-	        /*spokenMessage:*/ null,
-	        /*cardMessage:*/ null,
-	        /*audioUrl:*/ null,
-	        /*playBehavior:*/ null,
-	        /*token:*/ null,
-	        /*expectedPreviousToken:*/ null,
-	        /*shouldEndSession:*/ true,
-	        /*isStop:*/ false,
-	        /*isClearQueue:*/ false);
-	}
+  static respondWithEndSession(context) {
+    this.respond(
+      /*context:*/ context,
+      /*spokenMessage:*/ null,
+      /*cardMessage:*/ null,
+      /*audioUrl:*/ null,
+      /*playBehavior:*/ null,
+      /*token:*/ null,
+      /*expectedPreviousToken:*/ null,
+      /*shouldEndSession:*/ true,
+      /*isStop:*/ false,
+      /*isClearQueue:*/ false);
+  }
 
-	static respond(
-	    context,
-	    spokenMessage,
-	    cardMessage,
-	    audioUrl,
-	    playBehavior,
-	    token,
-	    expectedPreviousToken,
-	    shouldEndSession,
-	    isStop,
-	    isClearQueue) {
+  static respond(
+    context,
+    spokenMessage,
+    cardMessage,
+    audioUrl,
+    playBehavior,
+    token,
+    expectedPreviousToken,
+    shouldEndSession,
+    isStop,
+    isClearQueue) {
 
-	    context.succeed(
-	        this.buildResponseJSON(
-	            spokenMessage,
-	            cardMessage,
-	            audioUrl,
-	            playBehavior,
-	            token,
-	            expectedPreviousToken,
-	            shouldEndSession,
-	            isStop,
-	            isClearQueue));
-	}
+    context.succeed(
+      this.buildResponseJSON(
+        spokenMessage,
+        cardMessage,
+        audioUrl,
+        playBehavior,
+        token,
+        expectedPreviousToken,
+        shouldEndSession,
+        isStop,
+        isClearQueue));
+  }
 
-	static buildResponseJSON(
-	    spokenMessage,
-	    cardMessage,
-	    audioUrl,
-	    playBehavior,
-	    token,
-	    expectedPreviousToken,
-	    shouldEndSession,
-	    isStop,
-	    isClearQueue) {
+  static buildResponseJSON(
+    spokenMessage,
+    cardMessage,
+    audioUrl,
+    playBehavior,
+    token,
+    expectedPreviousToken,
+    shouldEndSession,
+    isStop,
+    isClearQueue) {
 
-	    var responseBody = {
-	        version: "1.0",
-	        // sessionAttributes: {}
-	        response: {}
-	    };
+    var responseBody = {
+      version: '1.0',
+      // sessionAttributes: {}
+      response: {}
+    };
 
-	    if (spokenMessage) {
-	        responseBody.response.outputSpeech =
-	            {
-	                type: "PlainText",
-	                text: spokenMessage,
-	            };
-	    }
+    if (spokenMessage) {
+      responseBody.response.outputSpeech =
+        {
+          type: 'PlainText',
+          text: spokenMessage,
+        };
+    }
 
-	    if (cardMessage) {
-	        responseBody.response.card =
-	            {
-	                type: "Simple",
-	                title: ALEXA_SKILL_NAME,
-	                content: cardMessage,
-	            };
-	    }
+    if (cardMessage) {
+      responseBody.response.card =
+        {
+          type: 'Simple',
+          title: ALEXA_SKILL_NAME,
+          content: cardMessage,
+        };
+    }
 
-	    if (audioUrl) {
-	        responseBody.response.directives = [
-	            {
-	                type: "AudioPlayer.Play",
-	                playBehavior: playBehavior || "REPLACE_ALL",
-	                audioItem: {
-	                    stream: {
-	                        token: token,
-	                        url: audioUrl,
-	                        offsetInMilliseconds: 0
-	                    }
-	                }
-	            }
-	        ];
+    if (audioUrl) {
+      responseBody.response.directives = [
+        {
+          type: 'AudioPlayer.Play',
+          playBehavior: playBehavior || 'REPLACE_ALL',
+          audioItem: {
+            stream: {
+              token: token,
+              url: audioUrl,
+              offsetInMilliseconds: 0
+            }
+          }
+        }
+      ];
 
-	        if (responseBody.response.directives[0].playBehavior === "ENQUEUE") {
-	            responseBody.response.directives[0].audioItem.stream.expectedPreviousToken = expectedPreviousToken;
-	        }
-	    }
+      if (responseBody.response.directives[0].playBehavior === 'ENQUEUE') {
+        responseBody.response.directives[0].audioItem.stream.expectedPreviousToken = expectedPreviousToken;
+      }
+    }
 
-	    if (isStop) {
-	        responseBody.response.directives = [
-	            {
-	                type: "AudioPlayer.Stop"
-	            }
-	        ];
-	    }
+    if (isStop) {
+      responseBody.response.directives = [
+        {
+          type: 'AudioPlayer.Stop'
+        }
+      ];
+    }
 
-	    if (isClearQueue) {
-	        responseBody.response.directives = [
-	            {
-	                type: "AudioPlayer.ClearQueue",
-	                clearBehavior: "CLEAR_ENQUEUED"
-	            }
-	        ];
-	    }
+    if (isClearQueue) {
+      responseBody.response.directives = [
+        {
+          type: 'AudioPlayer.ClearQueue',
+          clearBehavior: 'CLEAR_ENQUEUED'
+        }
+      ];
+    }
 
-	    responseBody.response.shouldEndSession = !!shouldEndSession;
+    responseBody.response.shouldEndSession = !!shouldEndSession;
 
-	    console.log(`>> RESPONSE BODY: ${JSON.stringify(responseBody)}`);
+    console.log(`>> RESPONSE BODY: ${JSON.stringify(responseBody)}`);
 
-	    return responseBody;
-	}
+    return responseBody;
+  }
 };
--- src/playlist.js
+++ src/playlist.js
@@ -1,95 +1,96 @@
-"use strict";
-var AlexaService = require("./alexa-service");
+'use strict';
+var AlexaService = require('./alexa-service');
 
 module.exports = class Playlist {
-    constructor(playlistId, currentIndex) {
-        this.playlistId = playlistId;
-        this.currentIndex = currentIndex;
-    }
+  constructor(playlistId, currentIndex) {
+    this.playlistId = playlistId;
+    this.currentIndex = currentIndex;
+  }
 
-    playFirst(event, context) {
-        this.currentIndex = 0;
-        this.buildAudioPlayerResponse(event, context, 0, true);
-    }
+  playFirst(event, context) {
+    this.currentIndex = 0;
+    this.buildAudioPlayerResponse(event, context, 0, true);
+  }
 
-    playPrevious(event, context) {
-        this.buildAudioPlayerResponse(event, context, -1, true);
-    }
+  playPrevious(event, context) {
+    this.buildAudioPlayerResponse(event, context, -1, true);
+  }
 
-    playNext(event, context) {
-        this.buildAudioPlayerResponse(event, context, 1, true);
-    }
+  playNext(event, context) {
+    this.buildAudioPlayerResponse(event, context, 1, true);
+  }
 
-    queuePrevious(event, context) {
-        this.buildAudioPlayerResponse(event, context, -1, false);
-    }
+  queuePrevious(event, context) {
+    this.buildAudioPlayerResponse(event, context, -1, false);
+  }
 
-    queueNext(event, context) {
-        this.buildAudioPlayerResponse(event, context, 1, false);
-    }
+  queueNext(event, context) {
+    this.buildAudioPlayerResponse(event, context, 1, false);
+  }
 
-    buildAudioPlayerResponse(event, context, indexOffset, isPlayImmediately) {
-        console.log("queue");
-        this.getSongDownloadUrls()
-            .then((urls) => {
-                if (urls[this.currentIndex + indexOffset]) {
-                    this.currentIndex += indexOffset;
-                    AlexaService.respond(
-                        /*context:*/ context,
-                        /*spokenMessage:*/ null,
-                        /*cardMessage:*/ null,
-                        /*audioUrl:*/ urls[this.currentIndex],
-                        /*playBehavior:*/ isPlayImmediately ? "REPLACE_ALL"  : "ENQUEUE",
-                        /*token:*/ this.getTokenJson(),
-                        /*expectedPreviousToken:*/ event.context.AudioPlayer.token,
-                        /*shouldEndSession:*/ true,
-                        /*isStop:*/ false,
-                        /*isClearQueue:*/ false);
-                } else {
-                    AlexaService.respond(
-                        /*context:*/ context,
-                        /*spokenMessage:*/ null,
-                        /*cardMessage:*/ null,
-                        /*audioUrl:*/ null,
-                        /*playBehavior:*/ null,
-                        /*token:*/ this.getTokenJson(),
-                        /*expectedPreviousToken:*/ null,
-                        /*shouldEndSession:*/ true,
-                        /*isStop:*/ false,
-                        /*isClearQueue:*/ false);
-                }
-            });
-    }
+  buildAudioPlayerResponse(event, context, indexOffset, isPlayImmediately) {
+    console.log('queue');
+    this.getSongDownloadUrls()
+      .then((urls) => {
+        if (urls[this.currentIndex + indexOffset]) {
+          this.currentIndex += indexOffset;
+          AlexaService.respond(
+            /*context:*/ context,
+            /*spokenMessage:*/ null,
+            /*cardMessage:*/ null,
+            /*audioUrl:*/ urls[this.currentIndex],
+            /*playBehavior:*/ isPlayImmediately ? 'REPLACE_ALL'  : 'ENQUEUE',
+            /*token:*/ this.getTokenJson(),
+            /*expectedPreviousToken:*/ event.context.AudioPlayer.token,
+            /*shouldEndSession:*/ true,
+            /*isStop:*/ false,
+            /*isClearQueue:*/ false);
+        } else {
+          AlexaService.respond(
+            /*context:*/ context,
+            /*spokenMessage:*/ null,
+            /*cardMessage:*/ null,
+            /*audioUrl:*/ null,
+            /*playBehavior:*/ null,
+            /*token:*/ this.getTokenJson(),
+            /*expectedPreviousToken:*/ null,
+            /*shouldEndSession:*/ true,
+            /*isStop:*/ false,
+            /*isClearQueue:*/ false);
+        }
+      });
+  }
 
-    getSongDownloadUrls() {
-        let songIds;
-        let downloadUrls;
-        let firebase = require('firebase');
-        let FIREBASE_CONFIG = require('./_no_commit/firebase-config.json');
-        let FIREBASE_USERNAME = FIREBASE_CONFIG.USER.USERNAME;
-        let FIREBASE_PASSWORD = FIREBASE_CONFIG.USER.PASSWORD;
-        // log-in to firebase
-        return firebase.auth().signInWithEmailAndPassword(FIREBASE_USERNAME, FIREBASE_PASSWORD)
-            // fetch songs in playlist
-            .then(() => firebase.database().ref(`playlists/${this.playlistId}/songs`).once('value'))
-            // extract songIds from songs in playlist
-            // fetch all songs
-            .then(snapshot => {
-                var playlistSongs = snapshot.val();
-                songIds = Object.keys(playlistSongs).map(playlistSongKey => playlistSongs[playlistSongKey].songId);
-                return firebase.database().ref('songs').once('value');
-            // use songIds to extract downloadUrls from songs
-            }).then(snapshot => {
-                var songs = snapshot.val();
-                downloadUrls = songIds.map(songId => songs[songId].downloadUrl);
-                return firebase.auth().signOut();
-            }).then(() => downloadUrls);
-    }
+  getSongDownloadUrls() {
+    let songIds;
+    let downloadUrls;
+    let firebase = require('firebase');
+    let FIREBASE_CONFIG = require('./_no_commit/firebase-config.json');
+    let FIREBASE_USERNAME = FIREBASE_CONFIG.USER.USERNAME;
+    let FIREBASE_PASSWORD = FIREBASE_CONFIG.USER.PASSWORD;
+    // log-in to firebase
+    return firebase.auth().signInWithEmailAndPassword(FIREBASE_USERNAME, FIREBASE_PASSWORD)
+      // fetch songs in playlist
+      .then(() => firebase.database().ref(`playlists/${this.playlistId}/songs`).once('value'))
+      // extract songIds from songs in playlist
+      // fetch all songs
+      .then(snapshot => {
+        var playlistSongs = snapshot.val();
+        songIds = Object.keys(playlistSongs).map(playlistSongKey => playlistSongs[playlistSongKey].songId);
+        return firebase.database().ref('songs').once('value');
+      })
+      // use songIds to extract downloadUrls from songs
+      .then(snapshot => {
+        var songs = snapshot.val();
+        downloadUrls = songIds.map(songId => songs[songId].downloadUrl);
+        return firebase.auth().signOut();
+      }).then(() => downloadUrls);
+  }
 
-    getTokenJson() {
-        return JSON.stringify({
-            playlistId: this.playlistId,
-            currentIndex: this.currentIndex
-        });
-    }
+  getTokenJson() {
+    return JSON.stringify({
+      playlistId: this.playlistId,
+      currentIndex: this.currentIndex
+    });
+  }
 };
