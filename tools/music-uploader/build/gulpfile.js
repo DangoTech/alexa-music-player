2b7cf5a3bb931e042e781e7a845d18d34aaf611a
monkeytacos <dickson.tse@gmail.com>
2017-01-28 11:13:41 -0800

    Updated music uploader tool to use dist folder
    added targets into package.json

--- tools/music-uploader/build/gulpfile.js
+++ tools/music-uploader/build/gulpfile.js
@@ -3,19 +3,20 @@
 const gulp = require('gulp');
 const del = require('del');
 const runSequence = require('run-sequence');
+const argv = require('yargs').argv;
 
 const BASE_DIR = '..';
 const PROJECT_DIR = '../../..';
-const BUILD_FOLDER = 'uploader';
-const BUILD_DIR = BUILD_FOLDER;
+const BUILD_FOLDER = 'dist';
 const CONFIG = 'config';
-const BUILD_CONFIG_DIR = BUILD_DIR + '/' + CONFIG;
 
-let BUILDMODE = 'test';
-let CONFIG_DIR = PROJECT_DIR + '/' + CONFIG + '/' + BUILDMODE;
+const BUILDMODE = argv.production ? 'production' : 'test';
+const BUILD_DIR = BUILD_FOLDER + '_' + BUILDMODE;
+const BUILD_CONFIG_DIR = BUILD_DIR + '/' + CONFIG;
+const CONFIG_DIR = PROJECT_DIR + '/' + CONFIG + '/' + BUILDMODE;
 
 gulp.task('clean', () => {
-  return del([BUILD_DIR], {force: true});
+  return del([BUILD_FOLDER + '*/'], {force: true});
 });
 
 gulp.task('src', () => {
